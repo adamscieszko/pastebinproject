bbec1593db674a90ccb022891b01a23a98eebb29
Adam <adam.scieszko@gmail.com>
2012-08-17 18:17:54 +0200

    final remarks

--- pastebinproject/urls.py
+++ pastebinproject/urls.py
@@ -15,4 +15,6 @@ urlpatterns = patterns('',
     # Uncomment the next line to enable the admin:
     url(r'^admin/', include(admin.site.urls)),
     url(r'^paste/', include('pastebin.urls')),
+    url(r'^static/(?P<path>.*)$', 'django.views.static.serve',
+         {'document_root': '/home/adam/PycharmProjects/pastebinproject/static/'}),
 )
